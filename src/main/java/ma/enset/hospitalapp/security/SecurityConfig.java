ab1f7fc8d57a7453a1c8c838e06bec6ec3f5c7aa
Babacar <babacar.diop@epitech.eu>
2023-10-12 12:34:43 +0200

    Config in memory authentication

---
+++ src/main/java/ma/enset/hospitalapp/security/SecurityConfig.java
@@ -0,0 +1,58 @@
+package ma.enset.hospitalapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+//    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager( ) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder().encode("1234")).roles("ADMIN","USER").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/",true).permitAll();
+        httpSecurity.rememberMe();
+        httpSecurity.authorizeHttpRequests().requestMatchers("/webjars/**").permitAll();
+
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/user/**").hasRole("USER");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN");
+
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+
+
+        httpSecurity.exceptionHandling().accessDeniedPage("/notAuthorized");
+
+        return httpSecurity.build();
+    }
+}
